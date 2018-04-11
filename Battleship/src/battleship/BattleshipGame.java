d38e7165b952a7ec08074c176ca1c7c85d5cbbb8
Sean Widmier <seanwidmier@gmail.com>
2018-04-11 16:42:26 -0500

    Preliminary work on BattleshipGame
    Added all of the methods + fields specified in class diagram, filled a few methods.
    
    Added accessors + mutators for friendlyShipSunkCount and enemyShipSunkCount, since it seemed necessary.
    
    Seems like this class may depend heavily on the methods from all the other classes. Such as when asking the name of the player, that needs to go through the GUI somehow, and we haven't determined that yet.
    
    May also want to create a "Fleet" class to aggregate Ships, instead of using the initializeAllShips() method that I've written.

--- Battleship/src/battleship/BattleshipGame.java
+++ Battleship/src/battleship/BattleshipGame.java
@@ -5,10 +5,75 @@
  */
 package battleship;
 
+import java.util.Scanner;
 /**
  *
  * @author Sean Widmier, Kyle Daigle, Kelly Manley, Robert Womack 
  */
-public class BattleshipGame {
+public class BattleshipGame extends Menu {
+    private String playerName;
+    private int friendlyShipsSunkCount;
+    private int enemyShipsSunkCount;
     
+    protected BattleshipGame(){
+        
+    }
+    
+    protected void initializeGame(){
+        System.out.println("Please enter your name: ");
+        Scanner input = new Scanner(System.in);
+        
+        playerName = input.nextLine();
+        greetPlayer();
+        
+        //Grid target = new Grid()    *****TODO*****
+        //Grid ocean = new Grid()     *****TODO*****
+        
+        initializeAllShips();
+    }
+    
+    private void initializeAllShips(){
+        Ship Carrier = new Ship("Carrier", 5);  // **** NEED TO REFERENCE PHYSICAL GAME RULES
+        Ship Battleship = new Ship("Battleship", 4);
+        Ship Cruiser = new Ship("Cruiser", 3);
+        Ship Submarine = new Ship("Submarine", 3);
+        Ship Destroyer = new Ship("Destroyer", 2);
+    }
+    
+    private void placeAllShips(){
+        
+    }
+    
+    protected void endGame(){
+        if(friendlyShipsSunkCount > enemyShipsSunkCount){
+            //showVictoryMessage();       *****TODO*****
+        }
+        else{
+            //showDefeatMessage();        *****TODO*****
+        }
+    }
+    
+    protected static void lockShips(){
+        
+    }
+    
+    private void greetPlayer(){
+        System.out.println("Greetings, Admiral " + playerName + "!");
+    }
+    
+    protected void addfriendlyShipSunk(){
+        friendlyShipsSunkCount++;
+    }
+    
+    protected void addenemyShipSunk(){
+        enemyShipsSunkCount++;
+    }
+    
+    protected int getFriendlyShipSunk(){
+        return friendlyShipsSunkCount;
+    }
+    
+    protected int getEnemyShipSunk(){
+        return enemyShipsSunkCount;
+    }
 }
