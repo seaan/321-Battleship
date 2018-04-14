ac1d9cdd44f27fd7bb0bd3baabe0d8c03e0240b8
Sean Widmier <seanwidmier@gmail.com>
2018-04-14 12:02:23 -0500

    add setPosition, startPosition, endPosition, Orientation enum, checkPosition
    startPosition and endPosition contain information regarding the start and end coordinates of the current ship. This allows us to check each grid position that the ship is located along when in checkPosition.
    The orientation enum contains easy information on whether the ship is vertical or horizontal.
    
    Check position will go to each position that the ship is located along and check to see if the coordinate given is within the ship.

--- Battleship/src/battleship/Ship.java
+++ Battleship/src/battleship/Ship.java
@@ -16,6 +16,14 @@ public class Ship {
     private static int hitCount;
     private static Boolean isSunk;
     
+    private Position startPosition;
+    private Position endPosition;
+    
+    Orientation orientation;
+    private enum Orientation{
+        HORIZONTAL, VERTICAL;
+    }
+    
     protected Ship(String type, int length) {
         shipType = type;
         shipLength = length;
@@ -42,5 +50,69 @@ public class Ship {
             isSunk = true;               
     }
     
+    protected void setPosition(int startX, int startY, int endX, int endY){
+        startPosition = new Position(startX, startY, Position.Status.SHIP);
+        endPosition = new Position(endX, endY, Position.Status.SHIP);
+        
+        if (this.startPosition.getX() == this.startPosition.getX())
+           orientation = Orientation.VERTICAL; 
+        else if (this.startPosition.getY() == this.startPosition.getY())
+           orientation = Orientation.HORIZONTAL;
+    }
     
+    protected boolean checkPosition(int x, int y){
+        if(x == this.startPosition.getX() && y == this.startPosition.getY())
+            return true;
+        else if(x == this.endPosition.getX() && y == this.endPosition.getY())
+            return true;
+        else{
+            switch(orientation){
+                case VERTICAL:
+                    if(x == this.startPosition.getX()){
+                        int topY = 0;
+                        int botY = 0;
+                        
+                        if(this.startPosition.getY() > this.endPosition.getY()){
+                            topY = this.startPosition.getY();
+                            botY = this.endPosition.getY();
+                        }
+                        else{
+                            topY = this.endPosition.getY();
+                            botY = this.endPosition.getY();
+                        }
+                        
+                        for(int i  = topY; i > botY; i--){
+                            if(y == i)
+                                return true;
+                        }
+                        return false;
+                    }
+                    
+                    return false;
+                    
+                case HORIZONTAL:
+                    if(x == this.startPosition.getX()){
+                        int topX = 0;
+                        int botX = 0;
+                        
+                        if(this.startPosition.getY() > this.endPosition.getX()){
+                            topX = this.startPosition.getX();
+                            botX = this.endPosition.getX();
+                        }
+                        else{
+                            topX = this.endPosition.getX();
+                            botX = this.endPosition.getX();
+                        }
+                        
+                        for(int i  = topX; i > botX; i--){
+                            if(x == i)
+                                return true;
+                        }
+                        return false;
+                    }
+                    return false;
+                    
+            }
+        }
+    }
 }
