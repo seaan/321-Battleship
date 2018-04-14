30939778369bb34e78234e3965dec382b4301a8c
Kyle Daigle <kdaigle23@gmail.com>
2018-04-14 10:34:16 -0500

    Added grids
    Added both grids as singletons. To call on these grids, use something like
    
    OceanGrid oGrid = OceanGrid.getInstance();
    
    Any grid called upon in the program will have the same data stored in it. This is so we can set the ships in the GUI and back end together more easily.

--- Battleship/src/battleship/BattleshipMain.java
+++ Battleship/src/battleship/BattleshipMain.java
@@ -14,7 +14,21 @@ public class BattleshipMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        // TEST 123
+        OceanGrid oGrid = OceanGrid.getInstance();
+        oGrid.setPeg(1, 9, 1);
+        oGrid.setPeg(1, 8, 1);
+        oGrid.setPeg(1, 7, 2);
+        oGrid.setPeg(1, 6, 1);
+        oGrid.setPeg(2, 9, 3);
+        oGrid.setPeg(3, 9, 3);
+        oGrid.setPeg(1, 5, 3);
+        oGrid.setPeg(1, 4, 3);
+        oGrid.setPeg(5, 5, 5);
+        
+        
+        OceanGrid testGrid = OceanGrid.getInstance();
+        System.out.println("Testing singleton grid: ");
+        testGrid.printGrid();
+        
     }
 }
