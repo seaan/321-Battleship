27dbf1f6d18abff020fc77973f612457f4331a9e
Sean Widmier <seanwidmier@gmail.com>
2018-04-18 16:10:59 -0500

    add OceanGUI, TargetGUI

--- Battleship/src/battleship/BattleshipMain.java
+++ Battleship/src/battleship/BattleshipMain.java
@@ -19,38 +19,8 @@ public class BattleshipMain {
      */
     public static void main(String[] args) {
         /* TESTING, DELETE */
-        bsg.initializeGame();
-
-        /* Carrier 5 */
-        Position startPosition = new Position(1, 1, Position.Status.SHIP);
-        Position endPosition = new Position(1, 5, Position.Status.SHIP);
-        bsg.updateShip(startPosition, endPosition, "Carrier");
-
-        /* Battleship 4 */
-        startPosition.setPosition(2, 1);
-        endPosition.setPosition(2, 4);
-        bsg.updateShip(startPosition, endPosition, "Battleship");
-
-        /* Cruiser 3 */
-        startPosition.setPosition(3, 1);
-        endPosition.setPosition(3, 3);
-        bsg.updateShip(startPosition, endPosition, "Cruiser");
-
-        /* Submarine 3 */
-        startPosition.setPosition(4, 1);
-        endPosition.setPosition(4, 3);
-        bsg.updateShip(startPosition, endPosition, "Submarine");
-
-        /* Destroyer 2 */
-        startPosition.setPosition(5, 1);
-        endPosition.setPosition(5, 2);
-        bsg.updateShip(startPosition, endPosition, "Destroyer");
+        OceanGUI = new OceanGUI();
         
-        Position pegPosition = new Position(1,1,Position.Status.HIT);
-        bsg.updatePeg(pegPosition, 0);
-        
-        bsg.ocean.printGrid();
-
 //        Launcher launcher = new Launcher();
 //        launcher.runGame();
     }
