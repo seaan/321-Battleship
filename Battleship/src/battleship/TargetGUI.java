88d8f0225de164ef9eced6028b4aefe9b0c3b786
R-Womack <35308111+R-Womack@users.noreply.github.com>
2018-04-22 15:36:57 -0500

    package update
    changed format so TargetGUI doesn't import so many individual packages

--- Battleship/src/battleship/TargetGUI.java
+++ Battleship/src/battleship/TargetGUI.java
@@ -1,28 +1,11 @@
 package battleship;
 
 import battleship.TargetGrid;
-import java.awt.BasicStroke;
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import javax.swing.BorderFactory;
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
 import javax.swing.border.EmptyBorder;
 
 /**
